0371f117c71b72950052b5dfbaa96b4b7f7f2a2b
Ironyi <120394481+Ironyi@users.noreply.github.com>
2025-08-28 12:22:05 -0400

    Fixed Contact page

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const message = this.querySelector("textarea").value;
-  
-    emailjs.send("service_3h7ty7t", "template_2xc852t", {
-      message: message
-    })
-    .then(() => {
-      alert("Message sent!");
-      this.reset();
-    }, (error) => {
-      alert("Failed to send. Try again later.");
-      console.error(error);
-    });
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = this.querySelector('input[name="name"]').value;
+  const email = this.querySelector('input[name="email"]').value;
+  const message = this.querySelector('textarea[name="message"]').value;
+
+  emailjs.send("service_3h7ty7t", "template_2xc852t", {
+    name: name,
+    email: email,
+    message: message
+  })
+  .then(() => {
+    alert("Message sent!");
+    this.reset();
+  }, (error) => {
+    alert("Failed to send. Try again later.");
+    console.error(error);
   });
+});
   
